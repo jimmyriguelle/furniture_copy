e45c918466728be401985905e92b7ae46f60ac4a
jimmyriguelle <jimmyriguelle@hotmail.com>
2021-12-22 05:54:44 +0100

    Initial commit

---
+++ lib/pages/product_detail_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ProductDetailPage extends StatefulWidget {
+  const ProductDetailPage(
+      {Key? key,
+      required String pPromotionPrice,
+      required String pPrice,
+      code,
+      img,
+      name,
+      id})
+      : super(key: key);
+
+  @override
+  _ProductDetailPageState createState() => _ProductDetailPageState();
+}
+
+class _ProductDetailPageState extends State<ProductDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
